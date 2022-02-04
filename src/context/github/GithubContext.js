0020adcd21d4964ba6801fc6dea4df5995b72d9c
Alyssa Voccia <avoccia@hotmail.com>
2022-02-03 21:52:52 -0500

    Added the ability to search users

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -13,16 +13,21 @@ export const GithubProvider = ({children}) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  // Get initial users (testing purposes)
-  const fetchUsers = async () => {
+  // Get search results
+  const searchUsers = async (text) => {
     setLoading();
-    const response = await fetch(`${GITHUB_URL}/users`);
 
-    const data = await response.json();
+    const params = new URLSearchParams({
+      q: text
+    })
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`);
+
+    const {items} = await response.json();
 
     dispatch({
       type: 'GET_USERS',
-      payload: data,
+      payload: items,
     })
   }
 
@@ -30,7 +35,7 @@ export const GithubProvider = ({children}) => {
   const setLoading = () => dispatch({type: 'SET_LOADING'});
 
   return (
-    <GithubContext.Provider value={{users: state.users, loading: state.loading, fetchUsers}}>
+    <GithubContext.Provider value={{users: state.users, loading: state.loading, searchUsers}}>
       {children}
     </GithubContext.Provider>
   )
