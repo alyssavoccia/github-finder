a3706b7cbb112331a60ba4d6f71a59ea8144ac34
Alyssa Voccia <avoccia@hotmail.com>
2022-02-06 13:39:33 -0500

    Added the User page and created function to get single user

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -8,6 +8,7 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 export const GithubProvider = ({children}) => {
   const initialState = {
     users: [],
+    user: {},
     loading: false
   };
 
@@ -28,8 +29,26 @@ export const GithubProvider = ({children}) => {
     dispatch({
       type: 'GET_USERS',
       payload: items,
-    })
+    });
   }
+
+    // Get single user
+    const getUser = async (login) => {
+      setLoading();
+  
+      const response = await fetch(`${GITHUB_URL}/users/${login}`);
+
+      if (response.status === 404) {
+        window.location = '/notfound';
+      } else {
+        const data = await response.json();
+  
+        dispatch({
+          type: 'GET_USER',
+          payload: data,
+        });
+      }
+    }
 
   // Clear users from state
   const clearUsers = () => dispatch({type: 'CLEAR_USERS'})
@@ -38,7 +57,7 @@ export const GithubProvider = ({children}) => {
   const setLoading = () => dispatch({type: 'SET_LOADING'});
 
   return (
-    <GithubContext.Provider value={{users: state.users, loading: state.loading, searchUsers, clearUsers}}>
+    <GithubContext.Provider value={{users: state.users, user: state.user, loading: state.loading, searchUsers, clearUsers, getUser}}>
       {children}
     </GithubContext.Provider>
   )
