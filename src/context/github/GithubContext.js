d1adac0e915583f1e1250e1a696a320ae8171854
Alyssa Voccia <avoccia@hotmail.com>
2022-02-15 14:36:23 -0500

    Moved searchUsers to new GithubActions file

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -15,24 +15,6 @@ export const GithubProvider = ({children}) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  // Get search results
-  const searchUsers = async (text) => {
-    setLoading();
-
-    const params = new URLSearchParams({
-      q: text
-    })
-
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`);
-
-    const {items} = await response.json();
-
-    dispatch({
-      type: 'GET_USERS',
-      payload: items,
-    });
-  }
-
   // Get single user
   const getUser = async (login) => {
     setLoading();
@@ -79,7 +61,15 @@ export const GithubProvider = ({children}) => {
   const setLoading = () => dispatch({type: 'SET_LOADING'});
 
   return (
-    <GithubContext.Provider value={{users: state.users, user: state.user, repos: state.repos, loading: state.loading, searchUsers, clearUsers, getUser, getUserRepos}}>
+    <GithubContext.Provider 
+      value={{
+        ...state,
+        dispatch,
+        clearUsers, 
+        getUser, 
+        getUserRepos
+      }}
+    >
       {children}
     </GithubContext.Provider>
   )
